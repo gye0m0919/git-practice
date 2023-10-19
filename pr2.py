eb43b68a66650baecfc814bf9f76da0f2d411e0c
gye0m0919 <144297872+gye0m0919@users.noreply.github.com>
2023-10-19 23:28:09 +0900

    Update pr2.py

--- pr2.py
+++ pr2.py
@@ -1,9 +1,1 @@
-userId = input('아이디를 입력하세요 : ')
-if userId == 'lovePython!':
-    usrPwd = input('비밀번호를 입력하세요 : ')
-    if usrPwd == 'p12345':
-        print('{}님환영합니다~!!'.format(userId)) 
-    else:  
-        print('비밀번호가틀립니다.') 
-else:  
-    print('아이디를찾을수없습니다.')
+#file fore git practice 2
